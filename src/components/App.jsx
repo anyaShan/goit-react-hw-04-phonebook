d3bd59d4a5e43ef894acdb21399f776a526ccd48
anya.shan <anya.shanaida@yahoo.com>
2022-11-01 19:32:58 +0200

    add utils

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,7 @@ import { Section } from 'components/Section/Section';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
-
-const INITIAL_CONTACTS = [
-  { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-  { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-  { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-  { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-];
+import { INITIAL_CONTACTS } from 'components/UtilsComponents/UtilsComponents';
 
 export function App() {
   // ======= заміна на useLocalStorage
